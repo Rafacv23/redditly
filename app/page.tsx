e9623df58a79f12064386ca455698dd075bd66bc
Rafa Canosa <rafacv23@gmail.com>
2024-09-12 16:28:49 +0200

    fixed layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,13 @@ import Nav from "@/components/Nav"
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-5 grid-rows-5 gap-4">
-      <div className="col-span-4 col-start-2 row-start-1">
-        <Header />
-      </div>
-      <div className="row-span-5 col-start-1 row-start-1">
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1">
         <Nav />
-      </div>
-      <div className="col-span-4 row-span-3 col-start-2 row-start-2">
         <Content />
       </div>
-      <div className="col-span-4 col-start-2 row-start-5">
-        <Footer />
-      </div>
+      <Footer />
     </div>
   )
 }
