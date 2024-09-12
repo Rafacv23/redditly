1e2d8330fabf2eb9d65a0c5f0ebbef68bc618aaf
Rafa Canosa <rafacv23@gmail.com>
2024-09-12 16:29:02 +0200

    styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer>&copy; 2023 Reddit Client. All rights reserved.</footer>
+  return (
+    <footer className="p-4">
+      &copy; 2023 Reddit Client. All rights reserved.
+    </footer>
+  )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,21 @@
 export default function Nav() {
-  return <div>nav</div>
+  return (
+    <div className="w-1/5 p-4 sticky top-0 h-screen">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <form>
+        <input type="text" placeholder="Search..." />
+      </form>
+    </div>
+  )
 }
