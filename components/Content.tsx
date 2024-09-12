d6d3ad9a1b72a12f7786f00b124ca5ac00119075
Rafa Canosa <rafacv23@gmail.com>
2024-09-12 16:28:54 +0200

    fetch

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,8 +1,27 @@
-export default function Content() {
+import Post, { PostData } from "@/components/Post"
+
+export interface RedditPost {
+  data: PostData
+}
+
+export default async function Content() {
+  const subreddit = `f1manager`
+
+  const res = await fetch(`https://www.reddit.com/r/${subreddit}.json`)
+
+  const data = await res.json()
+
+  const redditPosts: RedditPost[] = data.data.children
+
+  console.log(redditPosts)
+
   return (
-    <div>
-      <h1>Content Page</h1>
-      <p>This is the content page.</p>
+    <div className="flex-1 p-4">
+      <ul className="space-y-4">
+        {redditPosts.map((post) => (
+          <Post key={post.data.id} post={post.data} />
+        ))}
+      </ul>
     </div>
   )
 }
