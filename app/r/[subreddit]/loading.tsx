ef876f6b3199bb27698a08930ea81524fcfb8959
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 17:29:54 +0200

    add loading

---
+++ app/r/[subreddit]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  )
+}
