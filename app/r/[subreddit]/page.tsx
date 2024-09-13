265a25cd7a2aeea959f531e6ee222362b0143756
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 17:29:48 +0200

    add fallback

--- app/r/[subreddit]/page.tsx
+++ app/r/[subreddit]/page.tsx
@@ -1,9 +1,15 @@
 import Content from "@/components/Content"
+import { Suspense } from "react"
+import Loading from "./loading"
 
 export default async function Subreddit({
   params,
 }: {
   params: { subreddit: string }
 }) {
-  return <Content subreddit={params.subreddit} />
+  return (
+    <Suspense fallback={<Loading />}>
+      <Content subreddit={params.subreddit} />
+    </Suspense>
+  )
 }
