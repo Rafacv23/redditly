c16f5f6aa3156af29a45044b0667ee55dca30f95
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 13:17:11 +0200

    drawer content to be global in both apps

---
+++ components/ui/DrawerContent.tsx
@@ -0,0 +1,13 @@
+export default function DrawerContent() {
+  return (
+    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+      Subreddits
+      <li>
+        <a>Sidebar Item 1</a>
+      </li>
+      <li>
+        <a>Sidebar Item 2</a>
+      </li>
+    </ul>
+  )
+}
