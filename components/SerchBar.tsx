6c72bce22c5a9e86610457dc20c1c3a676af9fbc
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 14:28:15 +0200

    submit

--- components/SerchBar.tsx
+++ components/SerchBar.tsx
@@ -1,8 +1,18 @@
 "use client"
 
 import { Search } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState<string>("")
+  const router = useRouter()
+
+  const handleSearchSubmit = async (e) => {
+    e.preventDefault()
+    router.push(`/r/${searchTerm}`)
+  }
+
   return (
     <div className="form-control">
       <button
@@ -19,12 +29,17 @@ export default function SearchBar() {
               esc
             </button>
           </form>
+          <form onSubmit={handleSearchSubmit}>
+            <button>Enviar</button>
+          </form>
           <label className="input input-bordered flex items-center gap-2 mt-4">
             <Search />
             <input
               type="text"
               className="grow"
               placeholder="What are you looking for?"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
           </label>
         </div>
