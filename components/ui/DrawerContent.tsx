79faf399be1ad4eed9c1db43dbc896b246d66533
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 15:18:04 +0200

    add accordion

--- components/ui/DrawerContent.tsx
+++ components/ui/DrawerContent.tsx
@@ -2,12 +2,29 @@ export default function DrawerContent() {
   return (
     <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
       Subreddits
-      <li>
-        <a>Sidebar Item 1</a>
-      </li>
-      <li>
-        <a>Sidebar Item 2</a>
-      </li>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" defaultChecked />
+        <div className="collapse-title text-xl font-medium">
+          Recently visited
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">Fav Subreddits</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">Fav Posts</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
     </ul>
   )
 }
