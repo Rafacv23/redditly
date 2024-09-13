b03952cb5acc66e2aae21fa5a65302847bb9742f
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 13:36:04 +0200

    mobile styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Drawer from "@/components/ui/Drawer"
 
 export default function Nav() {
   return (
-    <div className="w-1/5 p-0 lg:p-4 sticky top-14 h-screen">
+    <div className="lg:w-1/5 p-0 lg:p-4 sticky top-14 h-screen">
       <Drawer display="desktop" />
     </div>
   )
