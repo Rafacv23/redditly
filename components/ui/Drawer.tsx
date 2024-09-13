07b6449cbb2d45b8dd2cf86cc39fd410f8bf8a49
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 13:16:59 +0200

    drawer content to manage mobile and desktop devices

---
+++ components/ui/Drawer.tsx
@@ -0,0 +1,49 @@
+import DrawerContent from "./DrawerContent"
+
+export default function Drawer({ display }: { display: "desktop" | "mobile" }) {
+  return (
+    <>
+      {display === "mobile" ? (
+        <div className="drawer lg:hidden">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col items-center justify-center">
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button lg:hidden"
+            >
+              Open drawer
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <DrawerContent />
+          </div>
+        </div>
+      ) : (
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col items-center justify-center">
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button hidden"
+            >
+              Open drawer
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <DrawerContent />
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
