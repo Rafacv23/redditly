624cf3108f52a4e129b3a7f8f5d9f3bf1831f4ac
Rafa Canosa <rafacv23@gmail.com>
2024-09-13 13:23:29 +0200

    fixed sticky issue

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Drawer from "@/components/ui/Drawer"
 
 export default function Nav() {
   return (
-    <div className="w-1/5 p-0 lg:p-4 sticky top-0 h-screen">
+    <div className="w-1/5 p-0 lg:p-4 sticky top-14 h-screen">
       <Drawer display="desktop" />
     </div>
   )
