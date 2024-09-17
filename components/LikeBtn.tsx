3ce1226296b14fdf9c23a5faba38011fcbe7ba56
Rafa Canosa <rafacv23@gmail.com>
2024-09-17 18:38:29 +0200

    dynamical rendering for the like btn

--- components/LikeBtn.tsx
+++ components/LikeBtn.tsx
@@ -2,9 +2,11 @@
 
 import { useRedditStore } from "@/store/store"
 import { PostData } from "@/components/Post"
+import { Heart, HeartOff } from "lucide-react"
 
 export default function LikeBtn({ post }: { post: PostData }) {
   const addFavPost = useRedditStore((state) => state.addFavPost)
+  const favPosts = useRedditStore((state) => state.favPosts)
 
   const handleAddFavPost = async (post: PostData) => {
     const processedPost = {
@@ -17,7 +19,15 @@ export default function LikeBtn({ post }: { post: PostData }) {
   }
   return (
     <button className="btn" onClick={() => handleAddFavPost(post)}>
-      Fav
+      {favPosts.some((favPost) => favPost.id === post.id) ? (
+        <div className="tooltip tooltip-bottom" data-tip="Dislike">
+          <HeartOff className="hover:text-primary transition-colors" />
+        </div>
+      ) : (
+        <div className="tooltip tooltip-bottom" data-tip="Like">
+          <Heart className="hover:text-primary transition-colors" />
+        </div>
+      )}
     </button>
   )
 }
