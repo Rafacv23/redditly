159a8dd034ccf9cdd8653e0bf5d969bb92ba8ce8
Rafa Canosa <rafacv23@gmail.com>
2024-09-17 17:05:57 +0200

    add hero and searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import Content from "@/components/Content"
+import SearchBar from "@/components/SerchBar"
 
 export default function Home() {
-  return <Content subreddit="f1manager" />
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  )
 }
