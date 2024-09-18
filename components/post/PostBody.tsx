dbc810f4c95dc84b5ef96327eb64330583c09d73
Rafa Canosa <rafacv23@gmail.com>
2024-09-18 18:40:54 +0200

    remove unnecesary data

--- components/post/PostBody.tsx
+++ components/post/PostBody.tsx
@@ -10,16 +10,13 @@ export default function PostBody({ post }: { post: PostData }) {
           <source src={post.media.reddit_video.fallback_url} type="video/mp4" />
         </video>
       )}
-      {post.secure_media_embed?.content && (
-        <div
-          className="embed-container max-w-md"
-          dangerouslySetInnerHTML={{
-            __html: post.secure_media_embed.content,
-          }}
-        />
-      )}
       {post.url.endsWith(".jpg") || post.url.endsWith(".png") ? (
-        <img className="max-w-md" src={post.url} alt={post.title} />
+        <img
+          className="max-w-md"
+          src={post.url}
+          alt={post.title}
+          loading="lazy"
+        />
       ) : null}
     </div>
   )
