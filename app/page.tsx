38e81fef6660015e9403066b86ec15f6067502f7
Rafa Canosa <rafacv23@gmail.com>
2024-09-18 18:09:59 +0200

    global constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import SearchBar from "@/components/SerchBar"
+import { SITE_DESCRIPTION, SITE_NAME } from "@/site/config"
 
 export default function Home() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <h1 className="text-5xl font-bold">{SITE_NAME}</h1>
+          <p className="py-6">{SITE_DESCRIPTION}</p>
           <SearchBar />
         </div>
       </div>
