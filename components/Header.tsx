50838c8cbbe0086fd1b6c903471ab84e3ee8728a
Rafa Canosa <rafacv23@gmail.com>
2024-09-18 18:10:20 +0200

    global constants and theme toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import SearchBar from "@/components/SerchBar"
 import Drawer from "@/components/ui/Drawer"
+import { SITE_NAME } from "@/site/config"
 import Link from "next/link"
+import ThemeToggle from "@/components/ThemeToggle"
 
 export default function Header() {
   return (
@@ -8,44 +10,14 @@ export default function Header() {
       <div className="flex-1">
         <h1 className="hidden lg:btn lg:btn-ghost lg:text-xl">
           <Link href="/" title="Home screen">
-            Reddit Client
+            {SITE_NAME}
           </Link>
         </h1>
         <Drawer display="mobile" />
       </div>
       <div className="flex-none gap-2">
         <SearchBar />
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        <ThemeToggle />
       </div>
     </div>
   )
