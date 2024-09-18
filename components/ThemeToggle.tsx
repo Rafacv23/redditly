ccb07958792e4ceffaa8e22aa285fe14c2a7943d
Rafa Canosa <rafacv23@gmail.com>
2024-09-18 18:16:04 +0200

    fixed order for light and dark theme

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -18,8 +18,7 @@ export default function ThemeToggle() {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <circle cx="12" cy="12" r="5" />
-        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
       </svg>
       <svg
         className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
@@ -33,7 +32,8 @@ export default function ThemeToggle() {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        <circle cx="12" cy="12" r="5" />
+        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
       </svg>
     </label>
   )
