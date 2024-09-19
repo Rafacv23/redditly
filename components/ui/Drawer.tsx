4d9392743e598243a7c0728bf597ff457137354f
Rafa Canosa <rafacv23@gmail.com>
2024-09-20 01:48:34 +0200

    add favicon and site name

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -1,3 +1,4 @@
+import { SITE_NAME } from "@/site/config"
 import DrawerContent from "./DrawerContent"
 
 export default function Drawer({ display }: { display: "desktop" | "mobile" }) {
@@ -11,7 +12,8 @@ export default function Drawer({ display }: { display: "desktop" | "mobile" }) {
               htmlFor="my-drawer-2"
               className="btn btn-primary drawer-button 2xl:hidden"
             >
-              Reddit Client
+              <img src="/favicon.ico" alt="Redder logo" width={20} />
+              {SITE_NAME}
             </label>
           </div>
           <div className="drawer-side">
@@ -24,7 +26,7 @@ export default function Drawer({ display }: { display: "desktop" | "mobile" }) {
           </div>
         </div>
       ) : (
-        <div className="drawer 2xl:drawer-open">
+        <div className="drawer 2xl:drawer-open rounded-xl">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex flex-col items-center justify-center">
             <label
