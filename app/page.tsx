99d2ab0bf8739f3375b87555141b893686026f27
Rafa Canosa <rafacv23@gmail.com>
2024-09-20 01:48:54 +0200

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import { SITE_DESCRIPTION, SITE_NAME } from "@/site/config"
 export default function Home() {
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center">
+      <div className="hero-content text-start">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">{SITE_NAME}</h1>
+          <h1 className="text-4xl font-bold bg-gradient-to-r from-primary via-primary-200 to-primary-800 bg-clip-text text-transparent">
+            {SITE_NAME}
+          </h1>
           <p className="py-6">{SITE_DESCRIPTION}</p>
           <SearchBar />
         </div>
