1f4364d06f1b40db060ece113a44002657d0cc91
Rafa Canosa <rafacv23@gmail.com>
2024-09-20 01:49:34 +0200

    new name and description

--- site/config.ts
+++ site/config.ts
@@ -1,5 +1,6 @@
-export const SITE_NAME = "Reddit Client"
-export const SITE_DESCRIPTION = "Esto es la descripción"
+export const SITE_NAME = "Redder"
+export const SITE_DESCRIPTION =
+  "Access the latest threads, comments, and trending content on Reddit in real time with our fast and easy-to-use app. Dive into the most interesting discussions and discover what's trending without any hassle. Your new way to browse Reddit starts here!"
 export const AUTHOR = "Rafa Canosa"
 export const GITHUB_URL = "https://github.com/Rafacv23/reddit-client"
 export const LINKEDIN_URL = "https://ninjapath.vercel.app/linkedin"
