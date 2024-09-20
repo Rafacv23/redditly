d15c6f0e12dd2f88454708f15ad0b42b6badc534
Rafa Canosa <rafacv23@gmail.com>
2024-09-21 01:16:29 +0200

    new format date function

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,32 +3,32 @@ export function formatSearchValue(searchValue: string): string {
 }
 
 export function formatDate(utcSeconds: number): string {
-  const postDate = new Date(utcSeconds * 1000) // Convertir de segundos a milisegundos
+  const postDate = new Date(utcSeconds * 1000) // Convert seconds to milliseconds
   const now = new Date()
   const diffInSeconds = Math.floor((now.getTime() - postDate.getTime()) / 1000)
 
   const secondsInMinute = 60
   const secondsInHour = 3600
   const secondsInDay = 86400
-  const secondsInMonth = 2592000 // Aproximadamente 30 días
-  const secondsInYear = 31536000 // Aproximadamente 365 días
+  const secondsInMonth = 2592000 // Approx 30 days
+  const secondsInYear = 31536000 // Approx 365 days
 
   if (diffInSeconds < secondsInMinute) {
-    return `hace ${diffInSeconds} segundos`
+    return `${diffInSeconds} second${diffInSeconds !== 1 ? "s" : ""} ago`
   } else if (diffInSeconds < secondsInHour) {
     const minutes = Math.floor(diffInSeconds / secondsInMinute)
-    return `hace ${minutes} minuto${minutes !== 1 ? "s" : ""}`
+    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`
   } else if (diffInSeconds < secondsInDay) {
     const hours = Math.floor(diffInSeconds / secondsInHour)
-    return `hace ${hours} hora${hours !== 1 ? "s" : ""}`
+    return `${hours} hour${hours !== 1 ? "s" : ""} ago`
   } else if (diffInSeconds < secondsInMonth) {
     const days = Math.floor(diffInSeconds / secondsInDay)
-    return `hace ${days} día${days !== 1 ? "s" : ""}`
+    return `${days} day${days !== 1 ? "s" : ""} ago`
   } else if (diffInSeconds < secondsInYear) {
     const months = Math.floor(diffInSeconds / secondsInMonth)
-    return `hace ${months} mes${months !== 1 ? "es" : ""}`
+    return `${months} month${months !== 1 ? "s" : ""} ago`
   } else {
     const years = Math.floor(diffInSeconds / secondsInYear)
-    return `hace ${years} año${years !== 1 ? "s" : ""}`
+    return `${years} year${years !== 1 ? "s" : ""} ago`
   }
 }
