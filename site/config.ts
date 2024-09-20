2b803bfe34b742372ee80c21ecd0e6c53c69caa6
Rafa Canosa <rafacv23@gmail.com>
2024-09-21 01:42:22 +0200

    new title

--- site/config.ts
+++ site/config.ts
@@ -1,4 +1,4 @@
-export const SITE_NAME = "Redder"
+export const SITE_NAME = "Redditly"
 export const SITE_DESCRIPTION =
   "Access the latest threads, comments, and trending content on Reddit in real time with our fast and easy-to-use app. Dive into the most interesting discussions and discover what's trending without any hassle. Your new way to browse Reddit starts here!"
 export const AUTHOR = "Rafa Canosa"
