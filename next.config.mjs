42e6aed1eb64ec7f9f407a027131b6c3308c292b
Rafa Canosa <rafacv23@gmail.com>
2024-09-21 17:14:27 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/fetch",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ]
+  },
+}
 
-export default nextConfig
+module.exports = nextConfig
