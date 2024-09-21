6f0340fbe642e08c4198cedbd216046b57785220
Rafa Canosa <rafacv23@gmail.com>
2024-09-21 02:00:06 +0200

    add revalidate

--- app/api/fetch/route.ts
+++ app/api/fetch/route.ts
@@ -2,6 +2,8 @@
 
 import { NextResponse } from "next/server"
 
+export const revalidate = 60
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const subreddit = searchParams.get("subreddit")
