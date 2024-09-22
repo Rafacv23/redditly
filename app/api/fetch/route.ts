53b40596c2bbc9aaeaa528d56ba70f562a88ace6
Rafa Canosa <rafacv23@gmail.com>
2024-09-23 00:19:35 +0200

    new api

--- app/api/fetch/route.ts
+++ app/api/fetch/route.ts
@@ -1,10 +1,16 @@
 // app/api/fetch/route.ts
 
 import { NextResponse } from "next/server"
+import { createRedditClient, SortingMethod } from "reddit-explorer"
 
 export const revalidate = 60
 
 export async function GET(request: Request) {
+  const reddit = createRedditClient({
+    clientId: "dBwNisc9LMsqrl9GQ726Xg",
+    secret: "OpzdSi00EwpJ5GFpBwylHmYidK26gw",
+  })
+
   const { searchParams } = new URL(request.url)
   const subreddit = searchParams.get("subreddit")
 
@@ -16,24 +22,28 @@ export async function GET(request: Request) {
   }
 
   try {
-    const res = await fetch(`https://www.reddit.com/r/${subreddit}.json`, {
-      headers: {
-        "User-Agent": "redditly/1.0.0 by rafacv23",
-      },
-    })
-    console.log("Reddit API response:", res)
+    const data = await reddit
+      .getSubreddit({
+        name: subreddit,
+        sortMethod: SortingMethod.Hot,
+      })
+      .then()
 
-    if (!res.ok) {
-      return NextResponse.json(
-        { error: "Failed to fetch subreddit" },
-        { status: res.status }
-      )
-    }
+    // const res = await fetch(`https://www.reddit.com/r/${subreddit}.json`, {
+    //   headers: {
+    //     "User-Agent": "redditly/1.0.0 by rafacv23",
+    //   },
+    // })
+    // console.log("Reddit API response:", res)
 
-    const data = await res.json()
-    const redditPosts = data.data.children
+    // if (!res.ok) {
+    //   return NextResponse.json(
+    //     { error: "Failed to fetch subreddit" },
+    //     { status: res.status }
+    //   )
+    // }
 
-    return NextResponse.json(redditPosts, { status: 200 })
+    return NextResponse.json(data.data.children, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error: "Error fetching data" }, { status: 500 })
   }
